52f0536b682c31549fe9fac0cae923e4a88e7d42
Danielg2004 <giraldoroldandanielg@gmail.com>
2026-08-17 23:28:32 -0500

    listado inicial de peliculas

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import Fastify from 'fastify';
 const app = Fastify();
 
+const peliculas = [
+  {
+    id: 1,
+    nombre: "SPIDER-MAN-BRAND-NEW-DAY",
+    duracion: 180,
+    genero: "ciencia ficcion",
+    descripcion: "pelicula de marvel"
+  }
+];
+
 app.get('/peliculas', async () => {
-  return { mensaje: 'peliculas de cine ' };
+  return { peliculas };
 });
 
 app.listen({ port: 3000 });
